56c4fa49c9d36083121e369572f59d2e49a371b7
Camin McCluskey <c.mccluskey_7@hotmail.co.uk>
2020-12-07 18:29:01 +0000

    Play around with redis and docker volumes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fetch = require('node-fetch');
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
+
+const Redis = require("ioredis");
+const redisClient = new Redis("redis://db:6379");
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 const QOTD_URL  = "http://quotes.rest/qod.json";
 const cannedResponse = {
@@ -31,25 +38,60 @@ const fetchQuote = async () => {
       const { author, quote } = res.contents.quotes[0];
       return {
         author,
-	quote
+				quote
       };
     })
     .catch(err => {
       console.log(err);
       return {
         author: null,
-	quote: null
+				quote: null
       };
     });
 };
 
 app.get('/', async (req, res) => {
-  const {author, quote } = await fetchQuote();
+	const { author, quote } = await fetchQuote();
   if (author && quote) {
-    res.send(quote + " - " + author);
+    res.send(`${quote} - ${author}`);
   } else {
     res.send("error");
   }
+});
+
+app.get('/saved', async (req, res) => {
+  try {
+		const keys = await redisClient.keys('*')
+		const randInt = Math.floor(Math.random() * keys.length); 
+		redisClient.get(keys[randInt], (err, resp) => {
+			if (err) throw err;
+			resp = JSON.parse(resp);
+			if (resp) {
+			  const { quote, author } = resp;
+				res.send(`${quote} - ${author}`);
+			} else {
+        console.error('no quotes available');
+				res.send('error - no quotes available. Add a quote by posting to /save');
+			}	
+		});
+	} catch (e) {
+		console.error(e);
+		res.send("error");
+	}
+});
+
+app.post('/save', async (req, res) => {
+	const { quote, author } = req.body;
+	if (quote && author) {
+		const resp = await redisClient.set(uuidv4(), JSON.stringify({ quote, author }));
+		if (resp === "OK") {
+			res.json({message: 'ok'});
+		} else {
+			res.json({message: 'err'});
+		}
+	} else {
+		res.json({message: 'bad request'});
+	}
 });
 
 app.listen(port, () => {
